4d7d7a4d98d3bd6bb7990c05086668e4af00fa19
BoofinBool <sioldead@mcmaster.ca>
2022-11-09 23:23:03 -0500

    Edited boxplot.py file. Created dataframes for test and train sets. Did class label pre-processing for pos and neg lables.

--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,18 @@
 from os import listdir
+
+import pandas as pd
 from pandas import read_csv
 import matplotlib.pyplot as plt
 from matplotlib import pyplot
 
-dataframe
+df_test = pd.read_csv("Data/aps_failure_test_set.csv")
+df_train = pd.read_csv("Data/aps_failure_training_set.csv")
+
+def get_correct_label(y):
+      return y.replace(['neg','pos'],[0,1])
+
+print(df_test['class'].unique())
+df_test['class'] = get_correct_label(df_test['class'])
+print(df_test['class'].unique())
+
+#df_test.boxplot()
