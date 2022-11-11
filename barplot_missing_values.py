d6c28aa51d3a06b9f867651353fdd0051c862ae6
Savvykltd <hidajats@mcmaster.ca>
2022-11-10 23:51:04 -0500

    barplot_missing_values.py is added to generate barplot of the features with missing values

--- barplot_missing_values.py
+++ barplot_missing_values.py
@@ -14,4 +14,11 @@ df_training['class'] = LE.fit_transform(df_training['class'])
 df_test['class'] = LE.fit_transform(df_test['class'])
 
 count_nan = {i:list(df_training.isna().sum()*100/df_training.shape[0])[j] for j,i in enumerate(df_training.columns)}
-count_nan
+count_nan = {i: n for i, n in sorted(count_nan.items(), key=lambda: item[1], reverse=True)}
+
+sb.set_style(style="whitegrid")
+plt.figure(figsize=(20,10))
+
+barplot = sb.barplot(x= list(count_nan.keys())[:15],y = list(count_nan.values())[:15], palette="hls")
+
+plt.show()
