9299022067898073187b85bf1d1b730f15c7c23d
rajottel <rajottel@mcmaster.ca>
2022-11-29 18:07:18 -0500

    modified code in RandomForestClassifier.py

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 # lines 8-10 from https://kearnz.github.io/autoimpute-tutorials/
 import warnings
 warnings.filterwarnings("ignore")
@@ -33,7 +34,20 @@ print_header("Results for RandomForestClassifier on full Dataset:")
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
 print("Recall:", metrics.recall_score(y_test, y_pred))
-
+# confusion matrix code from https://medium.com/analytics-vidhya/evaluating-a-random-forest-model-9d165595ad56
+full_confusion = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n", full_confusion)
+plt.figure(figsize=(15, 10))
+sb.set(font_scale=1.4)
+sb.heatmap(full_confusion, annot=True, annot_kws={'size':10}, cmap=plt.cm.Greens, linewidths=0.2)
+class_names = ['0','1']
+tick_marks = np.arange(len(class_names)) + 0.5
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.title('Confusion Matrix for Random Forest Model on full dataset')
+print(plt.show())
 # random sample code from https://datatofish.com/random-rows-pandas-dataframe/
 dataset1 = dataset.sample(frac=0.15)
 print_header("Dataset after random sampling")
@@ -51,3 +65,17 @@ print_header("Results for RandomForestClassifier on sampled Dataset:")
 print("Accuracy:", metrics.accuracy_score(y1_test, y1_pred))
 print("Precision:", metrics.precision_score(y1_test, y1_pred))
 print("Recall:", metrics.recall_score(y1_test, y1_pred))
+# confusion matrix code from https://medium.com/analytics-vidhya/evaluating-a-random-forest-model-9d165595ad56
+sample_confusion = confusion_matrix(y1_test, y1_pred)
+print("Confusion Matrix:\n", sample_confusion)
+plt.figure(figsize=(15, 10))
+sb.set(font_scale=1.4)
+sb.heatmap(sample_confusion, annot=True, annot_kws={'size':10}, cmap=plt.cm.Greens, linewidths=0.2)
+class_names = ['0','1']
+tick_marks = np.arange(len(class_names)) + 0.5
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.title('Confusion Matrix for Random Forest Model on sampled dataset')
+print(plt.show())
